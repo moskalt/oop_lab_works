72786f3acffcf4bacc72fdbce286edcbeede3ac3
Morozov312 <alexander.morozov312@gmail.com>
2020-10-06 19:08:05 +0700

    added clang-format

--- oop_lab_works.cpp
+++ oop_lab_works.cpp
@@ -1,14 +1,14 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode(1000, 1000), "Lab");
-  window.setFramerateLimit(60);
-  sf::Event event{};
-  while (window.isOpen()) {
-    while (window.pollEvent(event)) {
-      if (event.key.code == sf::Keyboard::Escape)
-        window.close();
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Lab");
+    window.setFramerateLimit(60);
+    sf::Event event{};
+    while (window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.key.code == sf::Keyboard::Escape)
+                window.close();
+        }
     }
-  }
-  return 0;
+    return 0;
 }
