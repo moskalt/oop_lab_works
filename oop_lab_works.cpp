665c605a651c874c113ae7f970e5da3cfc228a0b
Morozov312 <alexander.morozov312@gmail.com>
2020-10-14 22:41:51 +0700

    debug mode

--- oop_lab_works.cpp
+++ oop_lab_works.cpp
@@ -1,54 +1,46 @@
 #include "tpoint.h"
 #include <iostream>
+#define QTY_FIGURES 10
 
 int main() {
     srand(time(nullptr));
-    auto *pointsArray = new tPoint<sf::CircleShape>[20];
-    auto *circleArray = new tCircle<sf::CircleShape>[20];
-    auto *ellipseArray = new tEllipse<sf::CircleShape>[20];
+    auto *pointsArray = new tPoint<sf::CircleShape>[QTY_FIGURES];
+    auto *circleArray = new tCircle<sf::CircleShape>[QTY_FIGURES];
+    auto *ellipseArray = new tEllipse<sf::CircleShape>[QTY_FIGURES];
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "tPoint");
     window.setFramerateLimit(60);
     sf::Event event{};
     sf::Clock timer;
-    size_t check;
-    size_t random_object;
-    check = 1;
-    size_t counter = 0;
     while (window.isOpen()) {
-        counter++;
         while (window.pollEvent(event)) {
             if (event.key.code == sf::Keyboard::Escape) {
                 window.close();
             }
-            if (event.key.code == sf::Keyboard::F1) {
-                check = 2;
+            if (event.key.code == sf::Keyboard::Q) {
+                for (size_t i = 0; i < QTY_FIGURES; i++) {
+                    pointsArray[i].setRandomMovement();
+                    circleArray[i].setRandomMovement();
+                    ellipseArray[i].setRandomMovement();
+                }
             }
-            if (event.key.code == sf::Keyboard::F2) {
-                check = 1;
+            if (event.key.code == sf::Keyboard::W) {
+                for (size_t i = 0; i < QTY_FIGURES; i++) {
+                    pointsArray[i].setNormalMovement();
+                    circleArray[i].setNormalMovement();
+                    ellipseArray[i].setNormalMovement();
+                }
             }
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
-        if (counter % 50 == 0 && check == 2) {
-            for (size_t i = 0; i < 20; i++) {
-                random_object = rand() % 3;
-                if (random_object == 2) {
-                    pointsArray[i].setRandomMovement();
-                } else if (random_object == 1) {
-                    circleArray[i].setRandomMovement();
-                } else {
-                    ellipseArray[i].setRandomMovement();
-                }
-            }
-        }
         window.clear(sf::Color::Black);
-        for (size_t i = 0; i < 20; i++) {
-            pointsArray[i].setPoint(pointsArray[i].tickRateMove(pointsArray[i].getFigure()));
-            window.draw(pointsArray[i].getFigure());
-            circleArray[i].setCircle(circleArray[i].tickRateMove(circleArray[i].getCircle()));
+        for (size_t i = 0; i < QTY_FIGURES; i++) {
+            pointsArray[i].setPoint(pointsArray[i].objectMotion(pointsArray[i].getPoint()));
+            window.draw(pointsArray[i].getPoint());
+            circleArray[i].setCircle(circleArray[i].objectMotion(circleArray[i].getCircle()));
             window.draw(circleArray[i].getCircle());
-            ellipseArray[i].setEllipse(ellipseArray[i].tickRateMove(ellipseArray[i].getEllipse()));
+            ellipseArray[i].setEllipse(ellipseArray[i].objectMotion(ellipseArray[i].getEllipse()));
             window.draw(ellipseArray[i].getEllipse());
         }
         window.display();
--- tpoint.h
+++ tpoint.h
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 const int windowWidth = 1200;
 const int windowHeight = 700;
 
@@ -9,55 +10,61 @@ template<class T>
 class tPoint {
 private:
     // members
-    sf::Color m_color;
+    T m_point;
+    sf::Color m_PointColor;
+    float m_pointRadius = 3.f;
     float m_x;
     float m_y;
-    T figure;
-    float m_pointRadius = 3.f;
-    sf::Vector2f m_movementVar = {4.0, 4.0};
-
+    sf::Vector2f m_movementVar = {4.0, 0.0};
 
 public:
-    tPoint() {
-        m_color = {calcRandColor(), calcRandColor(), calcRandColor()};
-        m_x = getRandomNum(float(windowWidth) - m_pointRadius);
-        m_y = getRandomNum(float(windowHeight) - m_pointRadius);
-        figure.setFillColor(m_color);
-        figure.setRadius(m_pointRadius);
-        figure.setPosition(m_x, m_y);
-    };
-    float getRandomNum(int windowSize) {
-        windowSize -= getPointRadius() * 10;
-        return (float) (rand() % windowSize + getPointRadius()*10);
+    float getRandomNum(int windowSize, float size) {
+        float res = rand() % windowSize;
+        if (res <= size) {
+            res += size;
+        } else if (res >= (float) windowSize - size * 2) {
+            res -= size * 2;
+        }
+        return res;
     }
+    tPoint() {
+        m_PointColor = {calcRandColor(), calcRandColor(), calcRandColor()};
+        m_x = getRandomNum(windowWidth, m_pointRadius);
+        m_y = getRandomNum(windowHeight, m_pointRadius);
+        std::cout << m_x << " " << m_y << " point" << std::endl;
+        m_point.setFillColor(m_PointColor);
+        m_point.setRadius(m_pointRadius);
+        m_point.setPosition(m_x, m_y);
+    };
     static sf::Uint8 calcRandColor() {
         return rand() % 256;
     }
-    sf::CircleShape getFigure() {
-        return figure;
+    sf::CircleShape getPoint() {
+        return this->m_point;
     }
     float getPointRadius() {
         return this->m_pointRadius;
     };
     void setPoint(T temp) {
-        this->figure = temp;
+        this->m_point = temp;
     }
     void setRandomMovement() {
-        size_t random_x = rand() % 2;
-        size_t random_y = rand() % 2;
-        if (random_x == 0) {
-            m_movementVar.x = -1 * m_movementVar.x + random_x;
-        }
-        if (random_y == 0) {
-            m_movementVar.y = -1 * m_movementVar.y + random_y;
-        }
+        this->m_movementVar.x = 1 + random() % 6;
+        this->m_movementVar.y = 1 + random() % 6;
     }
-    T tickRateMove(T object) {
+    void setNormalMovement() {
+        this->m_movementVar.x = 4;
+        this->m_movementVar.y = 0;
+    }
+    virtual float getObjectSize() {
+        return this->m_pointRadius;
+    }
+    T objectMotion(T object) {
         sf::Vector2f currentPosition = object.getPosition();
-        if (currentPosition.x > windowWidth - object.getRadius() || currentPosition.x + object.getRadius() < 0) {
+        if (currentPosition.x > windowWidth - getObjectSize() * 2 || currentPosition.x <= 0) {
             m_movementVar.x = -1 * m_movementVar.x;
         }
-        if (currentPosition.y > windowHeight - object.getRadius() || currentPosition.y + object.getRadius() < 0) {
+        if (currentPosition.y > windowHeight - getObjectSize() * 2 || currentPosition.y <= 0) {
             m_movementVar.y = -1 * m_movementVar.y;
         }
         object.move(m_movementVar.x, m_movementVar.y);
@@ -73,17 +80,19 @@ private:
     float m_y;
     sf::Color m_color;
     sf::Color background_color;
+    float m_circleRadius = getPointRadius() * 10;
 
 public:
     tCircle() {
         m_color = {calcRandColor(), calcRandColor(), calcRandColor()};
         background_color = {0, 0, 0, 1};
         circle.setFillColor(background_color);
-        circle.setRadius(getPointRadius() * 10);
+        circle.setRadius(m_circleRadius);
         circle.setOutlineColor(m_color);
         circle.setOutlineThickness(2.f);
-        m_x = getRandomNum(float(windowHeight) - getPointRadius() * 10);
-        m_y = getRandomNum(windowWidth - getPointRadius() * 10);
+        m_x = getRandomNum(windowWidth, m_circleRadius);
+        m_y = getRandomNum(windowHeight, m_circleRadius);
+        std::cout << m_x << " " << m_y << " circle" << std::endl;
         circle.setPosition(m_x, m_y);
     }
 
@@ -93,9 +102,12 @@ public:
     void setCircle(T temp) {
         this->circle = temp;
     }
+    float getObjectSize() override {
+        return this->m_circleRadius;
+    }
 };
 
-template <class T>
+template<class T>
 class tEllipse : public tPoint<sf::CircleShape> {
 private:
     T ellipse;
@@ -103,18 +115,21 @@ private:
     float m_y;
     sf::Color m_color;
     sf::Color background_color;
+    float m_ellipseRadius = getPointRadius() * 10;
+    float m_scaleX = 1.5f;
 
 public:
     tEllipse() {
-        m_color = {calcRandColor(),calcRandColor(),calcRandColor()};
+        m_color = {calcRandColor(), calcRandColor(), calcRandColor()};
         background_color = {0, 0, 0, 1};
         ellipse.setFillColor(background_color);
-        ellipse.setRadius(getPointRadius() * 10);
-        ellipse.setScale(1.5f, 1.0f);
+        ellipse.setRadius(m_ellipseRadius);
+        ellipse.setScale(m_scaleX, 1.0f);
         ellipse.setOutlineColor(m_color);
         ellipse.setOutlineThickness(2.0f);
-        m_x = getRandomNum(float(windowHeight) - getPointRadius() * 10);
-        m_y = getRandomNum(windowWidth - getPointRadius() * 10);
+        m_x = getRandomNum(windowWidth, m_ellipseRadius);
+        m_y = getRandomNum(windowHeight, m_ellipseRadius);
+        std::cout << m_x << " " << m_y << " ellipse" << std::endl;
         ellipse.setPosition(m_x, m_y);
     }
     T getEllipse() {
@@ -122,5 +137,8 @@ public:
     }
     void setEllipse(T temp) {
         this->ellipse = temp;
+    }
+    float getObjectSize() override {
+        return this->m_ellipseRadius * this->m_scaleX;
     }
 };
